775c2af3c53ee04aa14cb4f85c64b309b58e4a93
truonghoangthuan <tthun32@gmail.com>
2021-04-21 14:39:03 +0700

    21/04/2021 14h40

---
+++ Lab03/Ex02/tacgia.sql
@@ -0,0 +1,10 @@
+--Tac gia: ma_tgia, ten_tgia
+
+INSERT INTO tacgia (ma_tgia, ten_tgia) VALUES (1, "Nhacvietplus");
+INSERT INTO tacgia (ma_tgia, ten_tgia) VALUES (2, "Sưu tầm");
+INSERT INTO tacgia (ma_tgia, ten_tgia) VALUES (3, "Sandy");
+INSERT INTO tacgia (ma_tgia, ten_tgia) VALUES (4, "Lê Trung Ngân");
+INSERT INTO tacgia (ma_tgia, ten_tgia) VALUES (5, "Khánh Ngọc");
+INSERT INTO tacgia (ma_tgia, ten_tgia) VALUES (6, "Night Stalker");
+INSERT INTO tacgia (ma_tgia, ten_tgia) VALUES (7, "Phạm Phương Anh");
+INSERT INTO tacgia (ma_tgia, ten_tgia) VALUES (8, "Tâm tình");
---
+++ Lab03/Ex02/theloai.sql
@@ -0,0 +1,10 @@
+--The loai: ma_tloai, ten_tloai
+
+INSERT INTO theloai VALUE (1, "Nhạc trẻ");
+INSERT INTO theloai VALUE (2, "Nhạc trữ tình");
+INSERT INTO theloai VALUE (3, "Nhạc cách mạng");
+INSERT INTO theloai VALUE (4, "Nhạc thiếu nhi");
+INSERT INTO theloai VALUE (5, "Nhạc quê hương");
+INSERT INTO theloai VALUE (6, "POP");
+INSERT INTO theloai VALUE (7, "Rock");
+INSERT INTO theloai VALUE (8, "R&B");
