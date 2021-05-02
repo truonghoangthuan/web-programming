07ca4d34660d41719913f3778555e7a6137130ce
truonghoangthuan <tthun32@gmail.com>
2021-05-02 21:19:35 +0700

    02/05/2021 21h20

--- Lab04/Ex2/baiviet-add-dropdown.php
+++ Lab04/Ex2/baiviet-add-dropdown.php
@@ -2,38 +2,32 @@
 
 <head>
     <meta charset="utf-8" />
+
+    <!-- for data picker -->
+    <link rel="stylesheet" href="resources/jquery.datepick/smoothness.datepick.css">
+    <script src="resources/jquery.datepick/jquery-1.10.2.js"></script>
+    <script src="resources/jquery.datepick/jquery.plugin.js"></script>
+    <script src="resources/jquery.datepick/jquery.datepick.js"></script>
+
+    <script>
+        $(function() {
+            $("#ngayviet").datepick({
+                dateFormat: 'yyyy/mm/dd'
+            });
+        });
+    </script>
+
+    <!-- END for date packer -->
+
 </head>
 
 <body>
-    <h1>Thêm bài viết</h1>
-    <hr>
-
-    <!-- form nhập thông tin bài viết -->
-    <form name="f_add" action="baiviet-add.php" method="post">
-        <pre>
-Mã bài viết <input type="text" size="5" name="ma_bviet" value="<?php include 'baiviet-add-func.php';
-                                                                echo get_ma_bviet(); ?>">
-    Tiêu đề <input type="text" size="70" name="tieude">
- Mã tác giả <input type="text" size="5" name="ma_tgia">
-  Ngày viết <input type="text" size="11" name="ngayviet" id="ngayviet">
-    Bài hát <input type="text" size="40" name="ten_bhat">
-Mã thể loại <input type="text" size="5" name="ma_tloai">
-    Tóm tắt <textarea rows="4"  cols="50" name="tomtat"></textarea>
-            <input type="submit" name="add_bviet" value="Thêm bài viết" onclick="return val();">
-        </pre>
-    </form>
-
-    <div class="message" id="add_msg"></div>
-    <hr>
-
-    <?php   /* xu ly them bai viet */
-
-    $msg = "";
+    <?php
     /* process add entry if any */
     if (isset($_POST['add_bviet'])) {
-
         //make connection and select DB
-        include("/php/web-programming/Lab03/Ex4/db-connector.php");
+        require_once("/php/web-programming/Lab03/Ex4/db-connector.php");
+
 
         $ma_bviet = $_POST['ma_bviet'];
         $tieude = $_POST['tieude'];
@@ -52,18 +46,48 @@ Mã thể loại <input type="text" size="5" name="ma_tloai">
             or die("Couldn't add new entry: " . mysqli_error($conn));
 
 
-        //echo "<br>$query<br>";
-
         //hien thi thong bao da them bai viet moi
         $msg = "Bài viết '" . $_POST['tieude'] . "' đã được thêm vào CSDL";
+        echo <<<_MESSAGE
+                <script type="text/javascript">
+                    document.getElementById("add_msg").innerHTML = "$msg";
+                </script>
+_MESSAGE;
     }   //them bai viet
 
     ?>
 
+    <h1>Thêm bài viết</h1>
+    <hr>
+
+    <!-- form nhập thông tin bài viết -->
+    <?php
+    include_once "baiviet-add-func.php";
+    ?>
+
+    <form name="f_add" action="baiviet-add-dropdown.php" method="post">
+        <pre>
+Mã bài viết <input type="text" size="5" name="ma_bviet" value="<? echo get_ma_bviet(); ?>">
+    Tiêu đề <input type="text" size="70" name="tieude">
+    Tác giả <select name="ma_tgia"><? dropdown_options('tacgia', 'ma_tgia', 'ten_tgia'); ?></select>
+  Ngày viết <input type="text" size="11" name="ngayviet" id="ngayviet">
+    Bài hát <input type="text" size="40" name="ten_bhat">
+   Thể loại <select name="ma_tloai"><? dropdown_options('theloai', 'ma_tloai', 'ten_tloai'); ?>
+</select>   
+    Tóm tắt <textarea rows="4"  cols="50" name="tomtat"></textarea>
+            <input type="submit" name="add_bviet" value="Thêm bài viết" onclick="return val();">
+        </pre>
+    </form>
+
+    <div class="message" id="add_msg"></div>
+    <hr>
+
     <!-- liet ke bai viet -->
     <?php
+
     //make connection and select DB
-    include("/php/web-programming/Lab03/Ex4/db-connector.php");
+    require_once("/php/web-programming/Lab03/Ex4/db-connector.php");
+
 
     //show all entries
     $query = "SELECT * FROM baiviet as bv, theloai as tl, tacgia as tg " .
@@ -72,8 +96,7 @@ Mã thể loại <input type="text" size="5" name="ma_tloai">
     $q_result = mysqli_query($conn, $query)
         or die("Query failed: " . mysqli_error($conn));
 
-    echo "<h3>Tổng số bài viết: " . mysqli_num_rows($q_result) . "</h3>";
-
+    echo "<h3>Tổng số bài viết: " . mysqli_fetch_array($q_result) . "</h3>";
 
     //generate list of entries
     $count = 1;
@@ -89,13 +112,6 @@ Mã thể loại <input type="text" size="5" name="ma_tloai">
         echo $row['ngayviet'];
         echo "</p>";
     }
-
-    echo <<<_MESSAGE
-                <script type="text/javascript">
-                    document.getElementById("add_msg").innerHTML = "$msg";
-                </script>
-_MESSAGE;
-
     ?>
 
     <!-- set default value for some controls -->
@@ -106,7 +122,7 @@ _MESSAGE;
 
     <script type="text/javascript">
         function val() {
-            //validation code, haven't finished yet
+            //validation code: haven't finished yet
             return true;
         }
     </script>
